9cb1dbd55e4b6bc26f632b29873007dba411f92f
nick.ramsbottom <nick.ramsbottom@hogarthww.com>
2017-07-25 08:56:13 +0100

    firebase deployment script added, README updated

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# okgoogle-cheesejokes
+# Cheese Jokes for Google Assistant
+
 Ask google assistant to tell you a joke about cheese.
+
+## Contributing
+
+Code contribution piece to come. If you have an incredible cheese joke you'd like to share make a pull request.
+
+## Hosting the backend
+
+This project uses firebase, install the cli with:
+
+`npm -i g firebase-tools`
+
+Login to firebase.
+
+`firebase login`
+
+Create a project on the following URL:
+
+`https://console.firebase.google.com`
+
+List your projects in the cli:
+
+`firebase list`
+
+Use the project you just made:
+
+`firebase use --add $(project ID from last step)`
+
+Deploy the backend:
+
+`firebase deploy`
+
+Make a note of your function URL.
---
+++ functions/index.js
@@ -0,0 +1,44 @@
+const functions = require('firebase-functions');
+const Assistant = require('actions-on-google').ApiAiAssistant;
+
+exports.cheeseJoke = functions.https.onRequest((req, res) => {
+  const questions = [
+    'What did the cheese say to itself in the mirror?',
+    'What do you call cheese that\'s not yours?',
+    'Did you hear about the explosion at the cheese factory?',
+    'What kind of cheese do you use to disguise a small horse?',
+  ];
+
+  const answers = [
+    'Halloumi',
+    'Nacho cheese',
+    'de Brie was everywhere',
+    'Mascarpone',
+  ];
+
+  const assistant = new Assistant({ request: req, response: res });
+
+  function questionHandler(assistant) {
+    const randomIndex = Math.floor(Math.random() * (questions.length));
+    const message = questions[randomIndex];
+    const contextObject = {
+      "randomIndex": randomIndex,
+    };
+
+    assistant.setContext('index', 1, contextObject);
+    assistant.ask(message);
+  }
+
+  function answerHandler(assistant) {
+    const context = assistant.getContext('index');
+    const fetchIndex = context.parameters.randomIndex;
+    const answer = answers[fetchIndex];
+
+    assistant.tell(answer);
+  }
+
+  const actionMap = new Map();
+  actionMap.set('ask', questionHandler);
+  actionMap.set('answer', answerHandler);
+  assistant.handleRequest(actionMap);
+});
