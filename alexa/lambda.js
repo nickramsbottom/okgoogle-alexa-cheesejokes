bb79e98b31e50e96f80767eb228349805b6b7dd2
nick.ramsbottom <nick.ramsbottom@hogarthww.com>
2017-07-27 08:58:07 +0100

    initial alexa commit

--- alexa/lambda.js
+++ alexa/lambda.js
@@ -1,3 +1,104 @@
 'use strict';
 
 const Alexa = require('alexa-sdk');
+
+const states = {
+  CALL: '_CALL',
+  REPLY: '_REPLY',
+};
+
+const jokes = {
+  calls: [
+    'What did the cheese say to itself in the mirror?',
+    'What do you call cheese that\'s not yours?',
+    'Did you hear about the explosion at the cheese factory?',
+    'What kind of cheese do you use to disguise a small horse?',
+  ],
+  replies: [
+    'Halloumi',
+    'Nacho cheese',
+    'de Brie was everywhere',
+    'Mascarpone',
+  ],
+};
+
+const strings = {
+  WELCOME_MESSAGE: 'some message',
+  STOP_MESSAGE: 'Goodbye!',
+  INSTRUCTIONS: 'instructions',
+};
+
+const newSessionHandlers = {
+  'NewSession': function () {
+    this.handle.state = states.CALL;
+  },
+
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', strings.STOP_MESSAGE);
+  },
+
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', strings.STOP_MESSAGE);
+  },
+};
+
+const callHandlers = Alexa.CreateStateHandler(states.CALL, {
+  'NewSession': function () {
+    this.emit('NewSession');
+  },
+
+  'tellJoke': function () {
+    const qsArr = jokes.calls;
+
+    this.handler.state = states.REPLY;
+
+    const rndIndex = Math.floor(Math.random() * qsArr.length);
+    this.attributes.index = rndIndex;
+
+    const question = qsArr[rndIndex];
+
+    this.emit(':ask', question);
+  },
+
+  'Unhandled': function () {
+    this.emit(':ask', strings.INSTRUCTIONS);
+  },
+
+  'AMAZON.HelpIntent': function () {
+    this.emit(':ask', strings.INSTRUCTIONS);
+  },
+
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', strings.STOP_MESSAGE);
+  },
+
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', strings.STOP_MESSAGE);
+  },
+});
+
+const replyHandlers = Alexa.CreateStateHandler(states.REPLY, {
+  'tellReply': function () {
+    const index = this.attributes.index;
+    const repsArr = jokes.replies;
+    this.emit(':tell', repsArr[index]);
+  },
+
+  'AMAZON.HelpIntent': function () {
+    this.emit(':ask', strings.INSTRUCTIONS);
+  },
+
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', strings.STOP_MESSAGE);
+  },
+
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', strings.STOP_MESSAGE);
+  },
+});
+
+exports.handler = function (event, context) {
+  const alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(newSessionHandlers, callHandlers, replyHandlers);
+  alex.execute();
+}
